e8607d7710c1d058a63dd60ed7bc2a1525a16e0a
amedeiros <amedeiros0920@gmail.com>
2015-03-07 11:30:45 -0500

    Create a pool for minions to swim in.

--- lib/parallel_minion.rb
+++ lib/parallel_minion.rb
@@ -2,7 +2,8 @@ require 'thread'
 require 'semantic_logger'
 
 module ParallelMinion
-  autoload :Minion,    'parallel_minion/minion'
+  autoload :Minion,  'parallel_minion/minion'
+  autoload :Pool,    'parallel_minion/pool'
 end
 
 require 'parallel_minion/railtie' if defined?(Rails)
